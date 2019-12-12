c2de982f1c565f24a28af0fdb577da59badf964d
Mike Beerens <beerens20@gmail.com>
2019-12-12 15:11:54 -0500

    Continued updating YelpCamp app

--- Yelp_Camp_Project/app.js
+++ Yelp_Camp_Project/app.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
 
+var campgrounds = [
+	{ name: "Salmon Creek", image: "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+	{ name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+	{ name: "Mountain Goat Rest", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+	{ name: "Salmon Creek", image: "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+	{ name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+	{ name: "Mountain Goat Rest", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" }
+];
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
@@ -10,12 +20,21 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-	var campgrounds = [
-		{ name: "Salmon Creek", image: "http://www.place-puppy.com/200x200" },
-		{ name: "Granite Hill", image: "http://www.place-puppy.com/200x200" },
-		{ name: "Mountain Goat Rest", image: "http://www.place-puppy.com/200x200" }
+	res.render("campgrounds", { campgrounds: campgrounds });
+});
 
-	];
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs")
+});
+
+app.post("/campgrounds", function(req, res){
+	// get data from from and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect("campgrounds");
 });
 
 // Tell Express to listen for requests (start server)
--- Yelp_Camp_Project/readme.md
+++ Yelp_Camp_Project/readme.md
@@ -1,16 +1,7 @@
 # YelpCamp
 
-* Add landing page  
-* Add campgrounds page that lists all campgrounds  
-
-Each campground has:  
-[ ] Name  
-[ ] Image
-
+###### Note about partials  
+New syntax  
 ```
-[
-	{name: "Salmon Creek", image: "http://www.image.com"},
-	{name: "Salmon Creek", image: "http://www.image.com"},
-	{name: "Salmon Creek", image: "http://www.image.com"}
-]
+<%- inlclude("partials/header") %>
 ```
