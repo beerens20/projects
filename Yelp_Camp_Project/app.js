8c3b1301eddd65bb7b84a383e17e5b3404cab0f9
Mike Beerens <beerens20@gmail.com>
2019-12-12 10:23:00 -0500

    Creating YelpCamp routes.

--- Yelp_Camp_Project/app.js
+++ Yelp_Camp_Project/app.js
@@ -1,22 +1,24 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-	res.send("Testing nodemon");
+	res.render("landing");
 });
 
-app.get("/images", function (req, res) {
-	request('https://jsonplaceholder.typicode.com/photos/1', function (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var parsedData = JSON.parse(body);
-			res.send(parsedData.title);
-		}
-	})
+app.get("/campgrounds", function (req, res) {
+	var campgrounds = [
+		{ name: "Salmon Creek", image: "http://www.place-puppy.com/200x200" },
+		{ name: "Granite Hill", image: "http://www.place-puppy.com/200x200" },
+		{ name: "Mountain Goat Rest", image: "http://www.place-puppy.com/200x200" }
+
+	];
 });
 
 // Tell Express to listen for requests (start server)
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
-	console.log("Movie App has started!!");
+	console.log("YelpCamp Server has started!!");
 });
---
+++ Yelp_Camp_Project/readme.md
@@ -0,0 +1,16 @@
+# YelpCamp
+
+* Add landing page  
+* Add campgrounds page that lists all campgrounds  
+
+Each campground has:  
+[ ] Name  
+[ ] Image
+
+```
+[
+	{name: "Salmon Creek", image: "http://www.image.com"},
+	{name: "Salmon Creek", image: "http://www.image.com"},
+	{name: "Salmon Creek", image: "http://www.image.com"}
+]
+```
