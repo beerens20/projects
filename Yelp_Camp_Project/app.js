1dc722eb00316cddadf2fb4b1ee90012ba29dd16
Mike Beerens <beerens20@gmail.com>
2019-12-30 08:29:44 -0500

    Update app.js
    added comments

--- Yelp_Camp_Project/app.js
+++ Yelp_Camp_Project/app.js
@@ -91,23 +91,21 @@ app.get("/campgrounds/:id", function(req, res){
 				console.log(err);
 				res.redirect("/campgrounds");
 			} else {
+				// create new comment
 				Comment.create(req.body.comment, function(err, comment){
 					if(err){
 						console.log(err);
 					} else {
+						// connect new comment to campground
 						campground.comments.push(comment);
 						campground.save();
+						// redirect to campground show page
 						res.redirect(`/campgrounds/${campground._id}`);
 					}
 				});
 			}
 		});
-		// create new comment
-		// connect new comment to campground
-		// redirect to campground show page
 	});
-
-
 });
 
 // Tell Express to listen for requests (start server)
