05b89ab7f555cb9010f186de12e47f39dd4bb5c0
Michael Beerens <beerens20@gmail.com>
2021-10-19 12:55:34 -0400

    Updated with pyinputplus

--- housing_calculator/cashflow.py
+++ housing_calculator/cashflow.py
@@ -4,12 +4,7 @@ Helps budget housing after expenses
 
 import time
 import sys
-
-print('Checking file integrity...', end='')
-time.sleep(2)
-print('COMPLETE')
-time.sleep(1)
-
+import pyinputplus as pyip
 
 print('Welcome to the cashflow tool\n\nWhat is your name?')
 name = input()
@@ -26,23 +21,23 @@ while SELECTION != '5':
           '2 - Enter monthly expense\n'
           '3 - See monthly expense rollup\n'
           '4 - See cashflow statement\n5 - Exit')
-    SELECTION = input()
-    if SELECTION == '2':
+    SELECTION = pyip.inputNum(greaterThan=0, lessThan=6)
+    if SELECTION == 2:
         while True:
-            print('Enter an expense name -OR- enter to quit')
-            name = input()
+            #print('Enter an expense name -OR- enter to quit')
+            name = pyip.inputStr(prompt='Enter an expense name -OR- enter to quit: ', blank=True, strip=True)
             name = name.lower()
             if name == '':
                 break
-            print('Enter a monthly amount for ' + name)
-            amount = input()
+            #print('Enter a monthly amount for ' + name)
+            amount = pyip.inputNum(prompt='Enter a monthly amount for %s: ' % (name))
             expenses[name] = amount
-    elif SELECTION =='3':
+    elif SELECTION ==3:
         for value in expenses.values():
             expenses_total += int(value)
         print(F'Monthly Expenses: {str(expenses_total)}')
         for key, value in expenses.items():
             print(F'{str(key.capitalize())} : {value}')
-    elif SELECTION == '5':
+    elif SELECTION == 5:
         print('Exiting')
         sys.exit()
